69e71807edd1792907a770ca9c08642f8ee69d12
lukezheng2011 <lukezheng2011@gmail.com>
2025-09-16 09:25:44 -0400

    created sentence word counter
    tested using "william is a good boy"

---
+++ challenge1.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+for i in range(1000):
+    sentence = input("Create a sentence: ")
+
+    sensplit= sentence.split()
+
+    print(len(sensplit)) #use len to determine how long sentence is
+
+
+
+
---
+++ strings.py
@@ -0,0 +1,4 @@
+x = "william li is a good boy"
+y= x.split()
+z = y[0]
+print(y[4], y[5])
