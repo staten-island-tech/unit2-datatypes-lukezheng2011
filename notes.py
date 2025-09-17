2605bfedfa51716032df1560244c0bdd97a188df
lukezheng2011 <lukezheng2011@gmail.com>
2025-09-17 09:26:16 -0400

    aaaa
    aaaa

---
+++ notes.py
@@ -0,0 +1,21 @@
+""" friend_comes=True
+money = True
+
+def amovies(friend, money):
+    if friend == True and money == True:
+        print("Going to movies")
+    else:
+        print("No movies")
+
+amovies(friend_comes,money)
+ """
+
+for i in range(6767):
+    homework = input("True or False: you have homework to do: ")
+    def not_movies(homework):
+        if homework==False:
+            print("movies")
+        else:
+            print("do your hw")
+    not_movies(homework)
+
