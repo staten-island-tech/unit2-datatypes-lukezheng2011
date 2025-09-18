a2da9e68f39059f7083d8855b6c7d28995ceaf03
lukezheng2011 <lukezheng2011@gmail.com>
2025-09-18 09:26:34 -0400

    sadaewr
    sasd

---
+++ badtogreatservice.py
@@ -0,0 +1,5 @@
+
+
+cost = int(input("How much is the bill: "))
+
+service = input("How was your service? Enter:")
---
+++ oddoreven.py
@@ -0,0 +1,13 @@
+
+
+for i in range(9999999999999999999999):
+    axc = input("Enter your number: ")
+    WOAH = (int(axc)%2)
+    if WOAH==1:
+        print("Your number is odd... ")
+    else:
+        print("Your number is even... ")
+
+
+
+
