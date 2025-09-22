45f6cf189ca2b7c8dc6ba63bdf757434ee89bc7f
lukezheng2011 <lukezheng2011@gmail.com>
2025-09-22 09:26:49 -0400

    l

---
+++ allfactors.py
@@ -0,0 +1,20 @@
+
+
+factorslist = ["1"]
+
+def op(william):
+    for i in range(1, {william}):
+        if william % i ==0:
+            factorslist.append(i)
+    return factorslist
+
+
+william = int(input("Enter the positive integer that you are trying to find all possible factors of: "))
+print (factorslist)
+
+
+
+
+
+
+
